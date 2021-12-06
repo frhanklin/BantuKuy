e9a434784901cf7f49c38797a4250b274513403e
Arlan Kusuma Ardika <landarlan60@gmail.com>
2021-12-06 12:18:54 +0800

    Working on Donor's dashboard

---
+++ app/src/main/java/com/frhanklindevs/bantukuy/ui/donor/DonorDashboardFragment.kt
@@ -0,0 +1,21 @@
+package com.frhanklindevs.bantukuy.ui.donor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.frhanklindevs.bantukuy.R
+
+class DonorDashboardFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_donor_dashboard, container, false)
+    }
+
+
+}
