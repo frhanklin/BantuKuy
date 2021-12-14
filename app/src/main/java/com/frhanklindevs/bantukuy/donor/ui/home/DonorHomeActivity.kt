30bb396b73f7721746bb89eaef815fa4de6c9bdb
Arlan Kusuma Ardika <landarlan60@gmail.com>
2021-12-14 21:56:58 +0800

    Memperbarui bottom navigation dan UI Donation Box

--- app/src/main/java/com/frhanklindevs/bantukuy/donor/ui/home/DonorHomeActivity.kt
+++ app/src/main/java/com/frhanklindevs/bantukuy/donor/ui/home/DonorHomeActivity.kt
@@ -2,15 +2,26 @@ package com.frhanklindevs.bantukuy.donor.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.LiveData
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.frhanklindevs.bantukuy.R
 import com.frhanklindevs.bantukuy.databinding.ActivityDonorHomeBinding
+import com.frhanklindevs.bantukuy.donor.ui.fragments.DonorDashboardFragment
+import com.frhanklindevs.bantukuy.donor.ui.fragments.DonorDonationBoxFragment
+import com.frhanklindevs.bantukuy.donor.ui.fragments.search.DonorSearchFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+import androidx.annotation.NonNull
+import androidx.fragment.app.Fragment
+
 
 class DonorHomeActivity : AppCompatActivity() {
 
     private var _binding : ActivityDonorHomeBinding? = null
     private val binding get() = _binding!!
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +29,44 @@ class DonorHomeActivity : AppCompatActivity() {
         _binding = ActivityDonorHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setUpBottomNavigation()
+//        setUpBottomNavigation()
+
+        binding.bottomNavMain.setOnNavigationItemSelectedListener(bottomNavListener)
+
+
+
+        supportFragmentManager.beginTransaction().add(R.id.nav_host_main, fragment3, "3").hide(fragment3).commit()
+        supportFragmentManager.beginTransaction().add(R.id.nav_host_main, fragment2, "2").hide(fragment2).commit()
+
+        supportFragmentManager.beginTransaction().add(R.id.nav_host_main, fragment1, "1").commit()
+
+
+
+
         supportActionBar?.hide()
     }
+
+    private val bottomNavListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_tab_dashboard -> {
+                    supportFragmentManager.beginTransaction().hide(active).show(fragment1).commit()
+                    active = fragment1
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.nav_tab_search -> {
+                    supportFragmentManager.beginTransaction().hide(active).show(fragment2).commit()
+                    active = fragment2
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.nav_tab_donation_box -> {
+                    supportFragmentManager.beginTransaction().hide(active).show(fragment3).commit()
+                    active = fragment3
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
 
 
     private fun setUpBottomNavigation() {
@@ -36,5 +82,13 @@ class DonorHomeActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    companion object {
+        private val fragment1= DonorDashboardFragment()
+        private val fragment2= DonorSearchFragment()
+        private val fragment3= DonorDonationBoxFragment()
+        private var active : Fragment = fragment1
+
     }
 }
